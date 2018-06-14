43b7c833d3a4e142effb813be4ce92956cf2c664
Shuhei_YOSHIDA <s-yoshida@frontier.hokudai.ac.jp>
2018-06-15 00:23:12 +0900

    astar will be written to process pure graph

--- astar.py
+++ astar.py
@@ -1,7 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+"""
+A-star algorithm
+"""
+
 import numpy as np
+
+class node(object):
+    def __init__(self, ):
+        pass
+
+class edge(object):
+    def __init__(self, ):
+        pass
+
+class Astar(object):
+    def __init__(self, ):
+        pass
 
 if __name__ == '__main__':
     pass
